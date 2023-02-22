fb350e3d25d8bf9936cbed030a7b0428c1a1b1c6
larsgson <4819617+larsgson@users.noreply.github.com>
2023-02-22 18:03:02 +0100

    changed order of tabs

--- apps/diegesis-admin-client/src/components/TabsTabs.js
+++ apps/diegesis-admin-client/src/components/TabsTabs.js
@@ -8,7 +8,7 @@ export default function TabsTabs({selectedTabIndex, setSelectedTabIndex, selecte
     };
 
     return <Tabs size="small" value={selectedTabIndex} onChange={handleChange} aria-label="local/remote tabs">
-        <Tab label="Remote" id="simple-tab-0" aria-controls="simple-tabpanel-0"/>
-        <Tab label="Local" id="simple-tab-1" aria-controls="simple-tabpanel-1"/>
+        <Tab label="Local" id="simple-tab-0" aria-controls="simple-tabpanel-0"/>
+        <Tab label="Remote" id="simple-tab-1" aria-controls="simple-tabpanel-1"/>
     </Tabs>
 }
