3e14180e4e34c45036bd5ada22c5b381c8bdf404
Jeffrey J. Gonzales <jeffrey.gonzales3@orange.fr>
2023-02-23 11:20:48 +0100

    Remove “Hello World” content from the vue client app

--- apps/vue-client/src/views/HomeView.vue
+++ apps/vue-client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Demo page</h1>
+    <p>Demo content</p>
   </main>
 </template>
