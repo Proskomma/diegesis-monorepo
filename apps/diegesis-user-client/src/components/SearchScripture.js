7262d5a06222274614c96ad5c4c71c1a40cf123d
rolfheij-sil <rolf_heij@sil.org>
2023-02-23 12:08:04 +0100

    Search matches in single book

--- apps/diegesis-user-client/src/components/SearchScripture.js
+++ apps/diegesis-user-client/src/components/SearchScripture.js
@@ -2,48 +2,83 @@ import React, {useContext, useState, useEffect} from 'react';
 import {Typography, TextField, Grid} from "@mui/material";
 import AppLangContext from "../contexts/AppLangContext";
 import i18n from "../i18n";
+import DocSelector from "./DocSelector";
 
 export default function SearchScripture({pk}) {
 
     const appLang = useContext(AppLangContext);
     const searchTitle = i18n(appLang, "CONTROLS_SEARCH");
 
+    const [docId, setDocId] = useState("");
+    const [docMenuItems, setDocMenuItems] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
-
+    const [validQuery, setValidQuery] = useState(false);
     const [matches, setMatches] = useState([]);
+
+    const docName = d => {
+      return d.headers.filter(d => d.key === 'toc3')[0]?.value ||
+          d.headers.filter(d => d.key === 'h')[0]?.value ||
+          d.headers.filter(d => d.key === 'toc2')[0]?.value ||
+          d.headers.filter(d => d.key === 'toc')[0]?.value ||
+          d.headers.filter(d => d.key === 'bookCode')[0].value
+    }
+
+    useEffect(() => {
+      const docSetInfo = pk.gqlQuerySync(
+        `{
+          docSets {
+            documents(sortedBy:"paratext") {
+              id
+              headers { key value }
+            }
+          }
+        }`
+      );
+      
+      setDocId(docSetInfo.data.docSets[0].documents[0].id);
+      setDocMenuItems(docSetInfo.data.docSets[0].documents.map(d => ({id: d.id, label: docName(d)})));
+
+    }, []) // this function (likely) only needs to run on initialization. Is this a good way of making such a function?
     
     useEffect(() => {
-      const re = /^[HhGg]\d{3,4}$/;
+      const re = /^[hHgG]\d{3,4}$/; // Strong's number pattern
 
       if(!re.test(searchQuery))
       {
-        console.log("no match");
+        setValidQuery(false);
         return;
       }
 
-      const uCSearchQuery = searchQuery.toUpperCase;
-
+      setValidQuery(true);
       const strongAtts = "attribute/spanWithAtts/w/strong/0/";
-
       const query = 
         `{
-          documents(sortedBy:"paratext") {
-            cvMatching( withScopes:["${strongAtts}${uCSearchQuery}"]) {
+          document(id: "${docId}" ) {
+            cvMatching( withScopes:["${strongAtts}${searchQuery.toUpperCase()}"]) {
               scopeLabels text
             }
           }
         }`;
 
-        // Syntax like this should work when searching for multiple inputs
+        // Syntax like this should work when querying for multiple strong's numbers
         // cvMatching( withScopes:["${strongAtts}${searchQuery[0]}", "${strongAtts}${searchQuery[1]}"]) {
 
         const result = pk.gqlQuerySync(query);
 
-        console.log(result);
+        setMatches(result.data.document.cvMatching.map(d => (
+          {c: d.scopeLabels[1].replace(/\D/g,''), v: d.scopeLabels[2].replace(/\D/g,''), t: d.text}
+          )));
     }, [searchQuery]);
 
     return (
         <Grid container>
+            <Grid item xs={12} sm={4} md={7} lg={8}>
+              <DocSelector
+                  docs={docMenuItems}
+                  docId={docId}
+                  setDocId={setDocId}
+              />
+            </Grid>
             <Grid item xs={12} sm={4} md={3} lg={2}>
               <TextField
                 value={searchQuery}
@@ -58,8 +93,9 @@ export default function SearchScripture({pk}) {
             </Grid>
             <Grid item xs={12}>
                 {
+                  !validQuery ? <Typography>Enter valid Strong's number in search box...</Typography> :
                   matches.length === 0 ?
-                  <Typography>No results...</Typography> :
+                  <Typography>There are no occurrences of this Strong's number in the selected book...</Typography> :
                   <Typography>Results found, but we'll have to work on how to display them...</Typography>
                 }
             </Grid>
