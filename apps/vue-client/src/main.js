24f07170e7bde2d53024c0f1d15332f685e19028
Jeffrey J. Gonzales <jeffrey.gonzales3@orange.fr>
2023-03-23 13:05:09 +0100

    WIP query https://diegesis.bible/graphql in main.js file for test purposes

--- apps/vue-client/src/main.js
+++ apps/vue-client/src/main.js
@@ -1,9 +1,75 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import gql from 'graphql-tag'
+import { Proskomma } from 'proskomma-core'
 
-const app = createApp(App)
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://diegesis.bible/graphql',
+})
 
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const pk = new Proskomma([
+  {
+    name: "source",
+    type: "string",
+    regex: "^[^\\s]+$",
+  },
+  {
+    name: "project",
+    type: "string",
+    regex: "^[^\\s]+$",
+  },
+  {
+    name: "revision",
+    type: "string",
+    regex: "^[^\\s]+$",
+  },
+]);
+
+let pkResult = undefined
+
+const apolloResult = apolloClient.query({
+  query: gql`{
+      localEntry(source: "eBible", id:"engBBE", revision: "2020-04-17")  {
+          canonResource(type: "succinct") {
+              content
+          }
+      }
+  }`,
+  result ({data, loading}) {
+    if (!loading) {
+      pk.loadSuccinctDocSet(JSON.parse(data.localEntry.canonResource.content));
+      pkResult = pk.gqlQuerySync(`{ docSet (id:"eBible_engBBE_2020-04-17") {id document(bookCode:"TIT"){mainSequence {blocksText}}}}`);
+      console.log(pkResult)
+    }
+  },
+  error (error) {
+    console.error(error)
+  },
+})
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+const app = createApp({
+  render: () => h(App),
+})
+
+app.use(apolloProvider)
 app.use(router)
 
 app.mount('#app')
