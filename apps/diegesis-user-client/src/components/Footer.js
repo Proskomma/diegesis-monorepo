dcb06bc6a502a0f7c383d203f599c529f5424183
Mark Howe <mvahowe@gmail.com>
2023-04-07 18:27:00 +0200

    Use clientStructure content in footer

--- apps/diegesis-user-client/src/components/Footer.js
+++ apps/diegesis-user-client/src/components/Footer.js
@@ -3,6 +3,10 @@ import { useContext } from 'react';
 import AppLangContext from '../contexts/AppLangContext';
 import i18n from '../i18n';
 import { directionText, fontFamily} from "../i18n/languageDirection";
+import ReactMarkdown from 'react-markdown';
+import {gql, useQuery} from "@apollo/client";
+import Spinner from "./Spinner";
+import GqlError from "./GqlError";
 
 export default function Footer() {
 
@@ -11,15 +15,35 @@ export default function Footer() {
     const end = i18n(appLang,'FOOTER_END')
     const link = i18n(appLang,'FOOTER_LINK')
 
+    const queryString = `{
+  clientStructure {
+    footer(language: "es") {
+      body
+    }
+  }
+  }`;
+
+    const {loading, error, data} = useQuery(
+        gql`
+      ${queryString}
+    `
+    );
+
+    if (loading) {
+        return <Spinner/>;
+    }
+
+    if (error) {
+        return <GqlError error={error}/>;
+    }
+
+
     const linkStyles = {
         color: "#FFF"
     }
     return <Box dir={directionText(appLang)} id="footer" sx={{backgroundColor: "primary.main", color: "#FFF", p: 3}}>
-        <Typography variant="body2" style={{ fontFamily : fontFamily(appLang)}}>{start}
-            <a href="http://mvh.bible" target="_blank" rel="noreferrer" style={linkStyles}>MVH Solutions</a>
-            {end}
-            <a href="http://doc.proskomma.bible" target="_blank" rel="noreferrer" style={linkStyles}>{link}</a>.
-        </Typography>
-        <Typography variant="body2" style={{ fontFamily : fontFamily(appLang)}}>© MVH Solutions 2023</Typography>
+        <ReactMarkdown>
+            {!loading && !error && data.clientStructure.footer.body}
+        </ReactMarkdown>
     </Box>
 }
