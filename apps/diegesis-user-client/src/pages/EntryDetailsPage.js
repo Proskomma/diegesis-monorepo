fe768c6511da0350349cd874d8dd62e6877b015f
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-05-09 18:14:17 -0400

    package updated

--- apps/diegesis-user-client/src/pages/EntryDetailsPage.js
+++ apps/diegesis-user-client/src/pages/EntryDetailsPage.js
@@ -12,7 +12,7 @@ import i18n from "../i18n";
 import PageLayout from "../components/PageLayout";
 const { EntryDetailUI, FlexibleDesign } = DiegesisUI;
 const { BottomBackBtn, InfoGrid } = EntryDetailUI;
-const { FlexibleTopControls, FlexibleSectionDivider, FlexibleActionButtons, FlexibleBookResourceBox, FlexibleBottomActionButtons } = FlexibleDesign.FlexibleEntryDetailUI;
+const { FlexibleTopControls, FlexibleSectionDivider, FlexibleBookResourceBox, FlexibleBottomActionButtons } = FlexibleDesign.FlexibleEntryDetailUI;
 const { Typography, Box, Container, styled } = MuiMaterial;
 
 const cellsConfig = [
