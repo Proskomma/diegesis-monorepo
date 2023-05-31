ebdc031d379ac1bcfc8f3bb31b4a339f6f2ef129
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-05-31 18:20:40 -0500

    fixed dynamic routes register issue after fetching it from server

--- apps/diegesis-user-client/src/routes/AppRoutes.js
+++ apps/diegesis-user-client/src/routes/AppRoutes.js
@@ -1,4 +1,5 @@
-import { RouterProvider, createBrowserRouter, useRouteError } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes, useRouteError } from "react-router-dom";
 import { useAppContext } from "../contexts/AppContext";
 import MarkdownPage from "../pages/MarkdownPage";
 import ListPage from "../pages/ListPage";
@@ -9,6 +10,9 @@ import EntryDownloadPage from "../pages/EntryDownloadPage";
 import UIConfigPage from "../pages/UIConfigPage";
 import ProtectedRoute from "./ProtectedRoute";
 import LoginPage from "../pages/LoginPage";
+import { useMemo } from "react";
+
+
 
 function ErrorBoundary() {
     let error = useRouteError();
@@ -22,70 +26,83 @@ function ErrorBoundary() {
 
 export default function AppRoutes() {
     const { clientStructure } = useAppContext();
-    
-    const markdownPageRoutes = (structure) => {
-        let ret = [];
-        if (structure.urls) {
-            for (const url of structure.urls) {
-                if (url === 'list') {
-                    continue;
-                }
-                ret.push(
-                    {
-                        path: (url === 'home' ? '/' : `${url}`),
-                        element: <MarkdownPage url={url} />,
-                        errorElement: <ErrorBoundary />
+
+    const routeList = useMemo(() => {
+        const routeList = []
+        if (Array.isArray(clientStructure.urls) && clientStructure.urls.length > 0) {
+            const markdownPageRoutes = (structure) => {
+                let ret = [];
+                if (structure.urls) {
+                    for (const url of structure.urls) {
+                        if (url === 'list') {
+                            continue;
+                        }
+                        ret.push(
+                            {
+                                path: (url === 'home' ? '/' : `/${url}`),
+                                element: <MarkdownPage url={url} />,
+                                errorElement: <ErrorBoundary />
+                            }
+                        );
                     }
-                );
+                }
+                return ret;
             }
+            routeList.push(...markdownPageRoutes(clientStructure))
         }
-        return ret;
-    }
+        routeList.push(...[
+            {
+                path: '/',
+                element: <MarkdownPage url={'home'} />,
+            }, // added default route because GraphQL response can be delay.
+            {
+                path: "/list",
+                element: <ListPage />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/entry/details/:source/:entryId/:revision",
+                element: <EntryDetailPage />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/entry/browse/:source/:entryId/:revision",
+                element: <EntryBrowsePage />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/entry/search/:source/:entryId/:revision",
+                element: <EntrySearchPage />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/entry/download/:source/:entryId/:revision",
+                element: <EntryDownloadPage />,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/ui-config",
+                element: <ProtectedRoute roles={['admin']}>
+                    <UIConfigPage />
+                </ProtectedRoute>,
+                errorElement: <ErrorBoundary />
+            },
+            {
+                path: "/login",
+                element: <LoginPage />,
+                errorElement: <ErrorBoundary />
+            }
+        ])
+        return routeList
+    }, [clientStructure]);
 
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            element: <MarkdownPage url={'home'} />,
-        }, // added default route because GraphQL response can be delay.
-        ...markdownPageRoutes(clientStructure),
-        {
-            path: "/list",
-            element: <ListPage />,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/entry/details/:source/:entryId/:revision",
-            element: <EntryDetailPage />,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/entry/browse/:source/:entryId/:revision",
-            element: <EntryBrowsePage />,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/entry/search/:source/:entryId/:revision",
-            element: <EntrySearchPage />,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/entry/download/:source/:entryId/:revision",
-            element: <EntryDownloadPage />,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/ui-config",
-            element: <ProtectedRoute roles={['admin']}>
-                <UIConfigPage />
-            </ProtectedRoute>,
-            errorElement: <ErrorBoundary />
-        },
-        {
-            path: "/login",
-            element: <LoginPage />,
-            errorElement: <ErrorBoundary />
-        },
-    ]);
-
-    return (<RouterProvider router={router} />)
-}
+    return (
+        <BrowserRouter>
+            <Routes>
+                {routeList.map((route) => (
+                    <Route key={route.path} path={route.path} element={route.element} errorElement={route.errorElement} />
+                ))}
+            </Routes>
+        </BrowserRouter>
+    );
+};
