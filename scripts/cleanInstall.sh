a928e0b1a9242a3a1839dc51461f4f30a935f4fa
Jeffrey J. Gonzales <jeffrey.gonzales3@orange.fr>
2023-06-13 12:32:18 +0200

    Update debug_config.json and cleanInstall.sh script to take into account the vue-client app

--- scripts/cleanInstall.sh
+++ scripts/cleanInstall.sh
@@ -6,8 +6,8 @@ if [ $result != "scripts" ]; then
     exit
 fi
 echo "removing 'node_modules' directories and 'package-lock' files..."
-rm -rf ../node_modules ../apps/diegesis-server/node_modules ../apps/diegesis-admin-client/node_modules/ ../apps/diegesis-user-client/node_modules/ ../apps/diegesis-upload-client/node_modules/
-rm ../package-lock.json ../apps/diegesis-admin-client/package-lock.json ../apps/diegesis-user-client/package-lock.json ../apps/diegesis-upload-client/package-lock.json ../apps/diegesis-server/package-lock.json
+rm -rf ../node_modules ../apps/diegesis-server/node_modules ../apps/diegesis-admin-client/node_modules/ ../apps/diegesis-user-client/node_modules/ ../apps/diegesis-upload-client/node_modules/ ../apps/vue-client/node_modules/
+rm ../package-lock.json ../apps/diegesis-admin-client/package-lock.json ../apps/diegesis-user-client/package-lock.json ../apps/diegesis-upload-client/package-lock.json ../apps/diegesis-server/package-lock.json ../apps/vue-client/package-lock.json
 cd ..
 echo "########################"
 echo "installing modules..."
@@ -20,4 +20,6 @@ npm run build
 cd ../diegesis-user-client/
 npm run build
 cd ../diegesis-upload-client/
+npm run build
+cd ../vue-client/
 npm run build
