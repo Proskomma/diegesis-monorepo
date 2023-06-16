2fd3ce6cf7df60f3fa48f6d2c92b0367352c5c75
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-06-16 14:11:17 -0400

    added custom tab and it’s functionality tried to use ui-kit tabs but it’s not full-filling the requirement

--- apps/diegesis-user-client/src/pages/StaticUIConfigPage.js
+++ apps/diegesis-user-client/src/pages/StaticUIConfigPage.js
@@ -1,35 +1,57 @@
 import React, { useState } from 'react'
 import { useApolloClient } from '@apollo/client';
 import { useAppContext } from '../contexts/AppContext';
-import { Box, Container, Tab, Tabs, TabPanel } from '@mui/material';
+import { Container, Tabs, Tab } from '@mui/material';
+import { Close } from '@mui/icons-material';
+
+//#region 
+const CustomTab = ({ title, onClose }) => (
+    <Tab
+        label={title}
+        iconPosition={'end'}
+        {...(onClose && {
+            icon: (
+                <span onClick={onClose}>
+                    <Close color={'disabled'} />
+                </span>
+            ),
+        })}
+    />
+);
+
+const CustomTabs = ({ tabs }) => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleChangeTab = (_, index) => {
+        setActiveTab(index);
+    };
+
+    const handleCloseTab = (event, index) => {
+        event.stopPropagation();
+        setActiveTab((prev) => (prev === index ? prev - 1 : prev));
+    };
+
+    return (
+        <Tabs value={activeTab} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto">
+            {tabs.map((tab, index) => (
+                <CustomTab key={index} title={tab.title} onClose={(event) => handleCloseTab(event, index)} />
+            ))}
+        </Tabs>
+    );
+};
+//#endregion
 
 export default function StaticUIConfigPage() {
-    const [tab, setTab] = useState(0);
+    const [tabOptions, setTabOptions] = useState([{ title: 'Home' }, { title: 'List' }]);
+
     const gqlClient = useApolloClient();
     const { appLang, mutateState: mutateAppState, clientStructure } = useAppContext();
 
-    const onTabChange = (ev) => {
-        debugger
-        setTab(ev.target.value)
-    }
     return (
         <Container>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={tab} onChange={onTabChange}>
-                    <Tab label="Item One" />
-                    <Tab label="Item Two"  />
-                    <Tab label="Item Three" />
-                </Tabs>
-            </Box>
-            <TabPanel value={tab} index={0}>
-                Item One
-            </TabPanel>
-            <TabPanel value={tab} index={1}>
-                Item Two
-            </TabPanel>
-            <TabPanel value={tab} index={2}>
-                Item Three
-            </TabPanel>
+            <CustomTabs tabs={tabOptions} />
         </Container>
     )
 }
+
+
