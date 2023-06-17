ac580b2a009e922c7ea35966c90efe54d1d4ae13
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-06-17 14:54:17 -0400

    functionality to add page and designed it's layout, handled edge cases of add and remove pages, styled active tab

--- apps/diegesis-user-client/src/pages/StaticUIConfigPage.js
+++ apps/diegesis-user-client/src/pages/StaticUIConfigPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useApolloClient } from '@apollo/client';
 import { useAppContext } from '../contexts/AppContext';
 import { Container, Tabs, Tab, IconButton, Box, TextField, Grid, Typography, Divider, Paper } from '@mui/material';
-import { Close } from '@mui/icons-material';
+import { Close, AddCardTwoTone } from '@mui/icons-material';
 import { DiegesisUI } from '@eten-lab/ui-kit';
 const { MarkdownEditor } = DiegesisUI.FlexibleDesign;
 
@@ -22,7 +22,7 @@ const TabTitle = ({ title, deletable, handleCloseTab, index }) => {
     )
 }
 
-const CustomTabs = ({ tabs, deleteTab }) => {
+const CustomTabs = ({ tabs, addTab, deleteTab }) => {
     const [activeTab, setActiveTab] = useState(0);
 
     const handleChangeTab = (_, index) => {
@@ -38,11 +38,20 @@ const CustomTabs = ({ tabs, deleteTab }) => {
     };
 
     return (
-        <Tabs value={activeTab} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto">
+        <Tabs value={activeTab} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto" sx={{
+            '.Mui-selected': {
+                backgroundColor: 'aliceblue'
+            }
+        }}>
             {tabs.map((tab, index) => (
                 <Tab
                     key={index}
-                    label={<TabTitle {...tab} index={index} handleCloseTab={handleCloseTab} />}
+                    sx={{ cursor: 'pointer' }}
+                    label={
+                        typeof tab.title === 'string'
+                            ? <TabTitle {...tab} index={index} handleCloseTab={handleCloseTab} />
+                            : tab.title
+                    }
                 />
             ))}
         </Tabs>
@@ -52,7 +61,7 @@ const CustomTabs = ({ tabs, deleteTab }) => {
 
 export default function StaticUIConfigPage() {
 
-    const [pages, setPages] = useState([]);
+    const [pages, setPages] = useState([]); //[{title: 'Page 1', deletable: false}]
     const [languages, setLanguages] = useState([]);
     const { appLang, mutateState: mutateAppState, clientStructure } = useAppContext();
 
@@ -60,9 +69,19 @@ export default function StaticUIConfigPage() {
 
     useEffect(() => {
         if (Array.isArray(clientStructure.urlData)) {
+            const lastItemIdx = clientStructure.urlData.length - 1
             const options = clientStructure.urlData.map(to => ({ title: to.menuText, deletable: true }));
-            options[0].deletable = false;
-            options[options.length - 1].deletable = false;
+            if (options[0]) options[0].deletable = false;
+            if (options[lastItemIdx]) options[lastItemIdx].deletable = false;
+            if (lastItemIdx > -1) {
+                options.splice(lastItemIdx, 0, {
+                    title: (
+                        <IconButton onClick={addPage} size={'small'}>
+                            <AddCardTwoTone />
+                        </IconButton>
+                    ),
+                })
+            }
             setPages(options)
         }
         if (Array.isArray(clientStructure.languages)) {
@@ -70,23 +89,33 @@ export default function StaticUIConfigPage() {
         }
     }, [clientStructure]);
 
-    const deleteTab = (idx) => {
+    const deletePage = (idx) => {
         const clonedPages = [...pages]
         clonedPages.splice(idx, 1)
         setPages(clonedPages)
+    }
+
+    const addPage = (e) => {
+        e.stopPropagation();
+        setPages(prev => {
+            const clonedPages = [...prev]
+            const newPageIdx = clonedPages.length - 2
+            clonedPages.splice(newPageIdx < 0 ? 0 : newPageIdx, 0, { title: 'UN-TITLED', deletable: true })
+            return clonedPages
+        })
     }
 
     return (
         <Container>
             <Paper elevation={1}>
                 {/* pages */}
-                <CustomTabs tabs={pages} deleteTab={deleteTab} />
+                <CustomTabs tabs={pages} addTab={addPage} deleteTab={deletePage} />
                 <Divider></Divider>
             </Paper>
             <br />
             <Paper elevation={1}>
                 {/* languages */}
-                <CustomTabs tabs={languages} deleteTab={deleteTab} />
+                <CustomTabs tabs={languages} deleteTab={deletePage} />
                 <Divider></Divider>
             </Paper>
 
