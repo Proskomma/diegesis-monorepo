6b855d0ab2630576fd6eaac9e9109fc7aa2f47ae
hardikpoudeldev <hardikpoudelupwork@gmail.com>
2023-07-05 19:26:17 -0400

    updated remote entries page with latest diegesis flexible components and added it's basic layouts and controls

--- apps/diegesis-user-client/src/pages/RemoteEntries.js
+++ apps/diegesis-user-client/src/pages/RemoteEntries.js
@@ -1,55 +1,137 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Box, Container } from "@mui/material";
-import Header from '../components/admin/Header';
-import TabbedBody from '../components/admin/TabbedBody';
-import GqlLoading from "../components/GqlLoading";
+import { Container, IconButton, Paper, Typography } from "@mui/material";
+import { DeleteOutlineOutlined } from '@mui/icons-material';
 import { useEffect, useState } from "react";
-import { gql, useApolloClient } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+import { DiegesisUI } from '@eten-lab/ui-kit';
 import PageLayout from "../components/PageLayout";
+import { searchQuery } from "../lib/remoteSearch";
+import EntriesFilter from "../components/admin/EntriesFilter";
+import GqlLoading from "../components/GqlLoading";
+import GqlError from "../components/GqlError";
+const { EntriesDataTable } = DiegesisUI.FlexibleDesign.FlexibleEntriesListUI;
+
+//#region helper methods
+const getGQLQuery = ({ org = '', lang = '', term = '' }) => {
+    return searchQuery(
+        `query catalogEntries {
+            org(name: "%org%") {
+                id: name
+                fullName
+                contentType
+                catalogHasRevisions
+                canSync
+                catalogEntries%searchClause% {
+                    source
+                    transId
+                    languageCode
+                    title
+                    isLocal
+                    isRevisionLocal
+                    revision
+                }
+            }
+        }`,
+        org,
+        lang,
+        term
+    );
+}
+//#endregion
 
 export default function RemoteEntries({ url }) {
-    const gqlClient = useApolloClient();
-    const [selectedOrgIndex, setSelectedOrgIndex] = useState(0);
-    const [searchLang, setSearchLang] = useState('');
-    const [searchText, setSearchText] = useState('');
-    const [orgs, setOrgs] = useState([]);
+    const [dataTable, setDataTable] = useState({ cellsConfig: [], entries: [] });
+    const [gqlQueryParams, setGQLQueryParams] = useState({ org: '', lang: '', term: '' });
+    const { loading, error, data } = useQuery(gql`${getGQLQuery(gqlQueryParams)}`);
+
     useEffect(
         () => {
-            const doOrgs = async () => {
-                const result = await gqlClient.query({
-                    query: gql`{
-                      orgs {
-                        id: name
-                        canSync
-                        catalogHasRevisions
-                      }
-                    }`
-                });
-                setOrgs(result.data.orgs);
-            };
-            doOrgs();
+            const cellsConfig = [{
+                id: 'source',
+                numeric: false,
+                disablePadding: true,
+                label: 'Source',
+            },
+            {
+                id: 'id',
+                numeric: false,
+                disablePadding: false,
+                label: 'ID',
+            },
+            {
+                id: 'revision',
+                numeric: false,
+                disablePadding: false,
+                label: 'Revision',
+            },
+            {
+                id: 'languageCode',
+                numeric: false,
+                disablePadding: false,
+                label: 'Language',
+            },
+            {
+                id: 'title',
+                numeric: false,
+                disablePadding: false,
+                label: 'Title',
+            },
+            {
+                id: 'actions',
+                numeric: false,
+                disablePadding: true,
+                label: 'Actions',
+                render() {
+                    return (
+                        <IconButton>
+                            <DeleteOutlineOutlined />
+                        </IconButton>
+                    );
+                },
+            }]
+            setDataTable({ ...dataTable, cellsConfig });
         }, []);
-    return <PageLayout id="remote-entries-page" parentPath={url ?? window.location.pathname}>
+
+    useEffect(() => {
+        if (!Array.isArray(data?.org?.catalogEntries)) return;
+        const contentType = data.org.contentType;
+        const transformedEntries = data.org.catalogEntries.map(catalogEntry => {
+            return ({
+                source: catalogEntry.source || "unknown",
+                id: catalogEntry.transId,
+                languageCode: catalogEntry.languageCode,
+                title: catalogEntry.title,
+            })
+        })
+        setDataTable({ ...dataTable, entries: transformedEntries });
+    }, [data])
+
+    const onFilterChange = ({ term, lang, org }) => {
+        const newQuery = { ...gqlQueryParams };
+        if (typeof term !== 'undefined') newQuery.term = term;
+        if (typeof lang !== 'undefined') newQuery.lang = lang;
+        if (typeof org !== 'undefined') newQuery.org = org;
+        setGQLQueryParams(newQuery);
+    }
+
+    const parentPath = url ?? window.location.pathname
+
+    const renderConditionalContent = () => {
+        if (loading) return <GqlLoading />
+        if (error) return <GqlError error={error} />
+        if (dataTable.entries.length) return <EntriesDataTable {...dataTable} />
+        return <Paper sx={{ width: '100%', overflow: 'hidden', my: 5, py: 5 }}>
+            <Typography variant="h5" textAlign={'center'}>
+                Entries Not Found For Selected Filters!
+            </Typography>
+        </Paper>
+    }
+
+    return <PageLayout id="remote-entries-page" parentPath={parentPath}>
         <Container>
-            <Header
-                orgs={orgs.map(o => o.id)}
-                selectedOrgIndex={selectedOrgIndex}
-                setSelectedOrgIndex={setSelectedOrgIndex}
-                searchLang={searchLang}
-                setSearchLang={setSearchLang}
-                searchText={searchText}
-                setSearchText={setSearchText}
-            />
-            <Box id="body">
-                {orgs.length > 0 ?
-                    <TabbedBody
-                        selectedOrgRecord={orgs[selectedOrgIndex]}
-                        searchLang={searchLang}
-                        searchText={searchText}
-                    /> :
-                    <GqlLoading />
-                }
-            </Box>
+            <EntriesFilter onFilterChange={onFilterChange} parentPath={parentPath} />
+            <br />
+            {renderConditionalContent()}
         </Container>
     </PageLayout>
 }
