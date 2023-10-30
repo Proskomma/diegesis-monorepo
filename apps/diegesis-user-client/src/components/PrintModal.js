be07f908e5168621036647f0ee82966009ca20e2
danielc-n <npmdanielc-n@protonmail.com>
2023-10-30 11:21:25 +0100

    fix pagedjs

--- apps/diegesis-user-client/src/components/PrintModal.js
+++ apps/diegesis-user-client/src/components/PrintModal.js
@@ -122,7 +122,7 @@ export default function PrintModal({
     newPage.document.body.innerHTML = `<div id="paras">${paras}</div>`;
     newPage.document.head.innerHTML = "<title>Diegesis PDF Preview</title>";
     const script = document.createElement("script");
-    script.src = `${window.location.protocol}//${window.location.host}/static/pagedjs_0_4_3.min.js`;
+    script.src = `${window.location.protocol}//${window.location.host}/static/paged.polyfill.js`;
     newPage.document.head.appendChild(script);
     const style = document.createElement("style");
     style.innerHTML = pageCss;
